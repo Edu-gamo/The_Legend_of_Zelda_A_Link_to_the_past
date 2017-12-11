557ec1b86db4f154f81d18e79634988219e3732a
Ferran Illa Capellas <ferran.illa26@gmail.com>
2017-12-11 14:17:49 +0100

    Fent l'inventari (arxiu world.js)

--- js/world.js
+++ js/world.js
@@ -29,6 +29,8 @@ zelda.world = {
         this.load.spritesheet('health','img/hearts.png',7,7,4,0,1);
         this.load.spritesheet('magicBar','img/magicMeter.png',8,2);
         this.load.image('hudNumbersFont','img/HUDnumbers.png');
+        
+        
                
     },
     
@@ -39,31 +41,39 @@ zelda.world = {
         this.game.add.existing(this.link);
         
         //HUD
-        this.HUD = this.game.add.sprite(0,0,'HUD',0);
-        this.HUD.fixedToCamera = true;
-        this.HUD.isInDungeon = false;
-        this.HUD.item = this.game.add.sprite(40,23,'items',0);
-        this.HUD.item.fixedToCamera = true;
-        this.HUD.health = this.game.add.group();
-        this.HUD.health.fixedToCamera = true;
+        this.HUD = this.game.add.group();
+        this.hud_bg = this.HUD.create(0,0,'HUD',0);
+        this.hud_bg.isInDungeon = false;
+        this.item = this.HUD.create(40,23,'items',0);
+        this.healthDisplay = this.game.add.group();
         //Here we'll create 10 of them evenly spaced apart
         for (var i = 0; i < 10; i++){
             //  Create a heart inside of the 'health' group
-            var heart = this.HUD.health.create(i*8+161, 24, 'health',0);
+            //var heart = 
+            this.healthDisplay.create(i*8+161, 24, 'health',0);
         }
-        this.HUD.magicBar = this.game.add.group();
-        this.HUD.magicBar.fixedToCamera = true;
+        this.HUD.add(this.healthDisplay);
+        this.magicBar = this.game.add.group();
         for (var i = 0; i < 16; i++){
             //  Create a piece of magic inside of the 'magicBar' group
-            var magicPortion = this.HUD.magicBar.create(24, 53-i*2, 'magicBar',0);
+            //var magicPortion = 
+            this.magicBar.create(24, 53-i*2, 'magicBar',0);
         }
-        this.HUD.font = this.game.add.retroFont('hudNumbersFont',7,7,'0123456789',5,1,1);
+        this.HUD.add(this.magicBar);
+        this.fontItemsDisplay = this.game.add.retroFont('hudNumbersFont',7,7,'0123456789',5,1,1);
+        this.imageFromFont = this.game.add.image(65, 24, this.fontItemsDisplay);
+        this.HUD.add(this.imageFromFont);
+        this.HUD.fixedToCamera = true;
         
         //------------------------------TESTING---------------------------------
         this.link.checkWorldBounds=true;
         this.link.events.onOutOfBounds.add(this.changeZone,this,0);
         this.link.zone = 0; //0 = house, 1 = left, 2 = castle
         this.changingZone = false;
+        //--------------------------------
+        //inventari
+        this.enter = zelda.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+        this.showInventari = false;
         
     },
     
@@ -73,9 +83,36 @@ zelda.world = {
             //this.camera.x -= 1; //moure la camara a on toqui
             //al cap de un segon{ camera.follow i world.setBounds}
         }else{
-        this.setHudValues();
-        this.HUD.frame = Number(this.HUD.isInDungeon);
-        
+            this.setHudValues();
+            this.HUD.frame = Number(this.HUD.isInDungeon);
+
+            //inventari
+            if(this.enter.isDown && this.enter.downDuration(1) /*&& hud.isTweening = false*/){
+                this.showInventari = !this.showInventari;
+                console.log(this.showInventari);
+                if(this.showInventari){
+                    //pause
+
+                    //desattach el hud de la camara
+                    this.HUD.fixedToCamera = false;
+
+                    //tween de l'inventari i hud
+                    this.game.add.tween(this.HUD).to({y: this.HUD.y+224},200,null,true);
+                    //inventari--> this.game.add.tween();
+                    //tween on complete inputs de l'inventari
+                }
+                if(!this.showInventari){
+                    //tween de l'inventari i hud
+                    var hudTween = this.game.add.tween(this.HUD).to({y: this.HUD.y-224},200,null,true);
+                    //resume on complete
+                    hudTween.onComplete.add(function() {
+                        arguments[0].fixedToCamera = true;
+                    });
+
+
+
+                }
+            }
         }
     },
     
@@ -124,60 +161,61 @@ zelda.world = {
     
     setHudValues:function(){ //set magic bar, item Selected, number of rupies etc, hp...
         //item
-        this.HUD.item.frame = this.link.itemSelected;
+        this.item.frame = 1//;this.link.itemSelected;
         
         //health
-        for(var i = 0; i < this.HUD.health.children.length; ++i){
+        for(var i = 0; i < this.healthDisplay.children.length; ++i){
             if(i < this.link.hearts){//pinta algun cor
                 if(i < this.link.health/2){
-                    this.HUD.health.children[i].frame = 0;
+                    this.healthDisplay.children[i].frame = 0;
                     if(this.link.health%2 != 0){ //health is odd
                         if(i+1 == (this.link.health+1)/2){
-                            this.HUD.health.children[i].frame = 1;
+                            this.healthDisplay.children[i].frame = 1;
                         }
                     }
                 }else{
-                    this.HUD.health.children[i].frame = 2;
+                    this.healthDisplay.children[i].frame = 2;
                 }
             }else{
-                this.HUD.health.children[i].frame = 3;//no cor
+                this.healthDisplay.children[i].frame = 3;//no cor
             } 
         }
         
         //magic bar
-        for(var i = 0; i < this.HUD.magicBar.children.length; ++i){
+        for(var i = 0; i < this.magicBar.children.length; ++i){
             if(i < this.link.magic){
                 if(i == this.link.magic-1){
-                    this.HUD.magicBar.children[i].frame = 0;
+                    this.magicBar.children[i].frame = 0;
                 }else{
-                    this.HUD.magicBar.children[i].frame = 1;
+                    this.magicBar.children[i].frame = 1;
                 }
             }else{
-                this.HUD.magicBar.children[i].frame = 2;
+                this.magicBar.children[i].frame = 2;
             }
         }
+        
         
         //Number of objects (count)
         //rupees
         var auxString = String(this.link.rupees);
         while(auxString.length < 3) auxString = '0'+auxString;
-        this.HUD.font.text = auxString;
+        this.fontItemsDisplay.text = auxString;
         //bombs
         auxString = String(this.link.bombs);
         while(auxString.length < 2) auxString = '0'+auxString;
-        this.HUD.font.text += (' ' +auxString);
+        this.fontItemsDisplay.text += (' ' +auxString);
         //Arrows
         auxString = String(this.link.arrows);
         while(auxString.length < 2) auxString = '0'+auxString;
-        this.HUD.font.text += (' ' +auxString);
+        this.fontItemsDisplay.text += (' ' +auxString);
         //Keys
-        if(this.HUD.isInDungeon){
+        if(this.hud_bg.isInDungeon){
             auxString = String(this.link.keys);
-            this.HUD.font.text += (' ' +auxString);
+            this.fontItemsDisplay.text += (' ' +auxString);
         }
-        this.HUD.font.customSpacingX = 1;
-        var i = this.game.add.image(65, 24, this.HUD.font);
-        i.fixedToCamera = true;
+        this.fontItemsDisplay.customSpacingX = 1;
+        this.imageFromFont = this.game.add.image(65, 24, this.fontItemsDisplay);
+        //i.fixedToCamera = true;
         
     },
     
