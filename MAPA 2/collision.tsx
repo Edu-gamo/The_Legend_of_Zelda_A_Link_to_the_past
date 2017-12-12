0050dbcfbe0a29b731e6325f5d208b66f7fd5e92
Marc Perea i Agustí <marcpa95@gmail.com>
2017-12-12 17:40:35 +0100

    castle map beginning

---
+++ MAPA 2/collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="collision" tilewidth="8" tileheight="8" tilecount="1" columns="1">
+ <image source="../MAPA 1/collision.png" width="8" height="8"/>
+</tileset>
